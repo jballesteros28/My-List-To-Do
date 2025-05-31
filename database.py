5513e4dad1b7b1f66b81d5f0413f64eb81cf90a9
juan david ballesteros <juandavidballesterosbayona@gmail.com>
2025-05-30 23:41:22 -0300

    Migración a PostgreSQL para Render

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Usa variable de entorno para la URL de la base de datos
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://list_to_do_user:ps0pY8iSlykRIPYFk3FKxtZiXKZrvKRz@dpg-d0stkv3uibrs73apr38g-a/list_to_do")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
