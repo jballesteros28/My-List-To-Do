ef846181d0710ed11e8db3598e41ea68e548a1e1
juan david ballesteros <juandavidballesterosbayona@gmail.com>
2025-06-07 01:01:49 -0300

    solucionando errores del backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,36 @@
 import { useState } from "react";
 import TareaForm from "./components/TareaForm";
 import TareaList from "./components/TareaList";
+import { useAuth } from "./context/AuthContext";
+import LoginForm from "./components/LoginForm"; 
 import "./styles/App.css";
 
 function App() {
   const [actualizar, setActualizar] = useState(false);
-  const [tareaActual, setTareaActual] = useState(null); // 👈 NUEVO
+  const [tareaActual, setTareaActual] = useState(null);
 
   const refrescarLista = () => setActualizar(!actualizar);
+  const { token } = useAuth();
 
   return (
     <div className="app-container">
       <h1>Lista de Tareas</h1>
-      <TareaForm
-        onTaskSaved={refrescarLista}
-        tareaActual={tareaActual}
-        setTareaActual={setTareaActual}
-      />
-      <TareaList
-        actualizar={actualizar}
-        onEditClick={(tarea) => setTareaActual(tarea)}
-        onTaskChanged={refrescarLista}
-      />
+      {!token ? (
+      <LoginForm /> // Muestra el login si no hay token
+    ) : (
+      <>
+        <TareaForm
+          onTaskSaved={refrescarLista}
+          tareaActual={tareaActual}
+          setTareaActual={setTareaActual}
+        />
+        <TareaList
+          actualizar={actualizar}
+          onEditClick={(tarea) => setTareaActual(tarea)}
+          onTaskChanged={refrescarLista}
+        />
+      </>
+    )}
     </div>
   );
 }
